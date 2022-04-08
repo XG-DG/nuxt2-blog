6c322143ed801bfdcb9982f17be02780802db2c4
XG-DG <103256594+XG-DG@users.noreply.github.com>
2022-04-08 20:17:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
   title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  nickName: "小顾", // 昵称
+  githubName: 'XG-DG', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '情不知所起，一往而深',
+  SEO_keywords: 'XG-DG\'s blog,情不知所起，一往而深',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
